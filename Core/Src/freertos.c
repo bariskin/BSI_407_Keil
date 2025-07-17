2133967c60a951b5d904f721c67fa6ea2cb09511
bariskin <chiurcciu@mail.ru>
2025-07-17 17:43:10 +0300

    Тестовая рабочая версия для Holding и Input регистров

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -31,7 +31,7 @@
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-
+uint8_t SelectaFlag = 0;
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
@@ -54,6 +54,14 @@ osStaticThreadDef_t defaultTaskControlBlock;
 osThreadId MasterModbusTasHandle;
 uint32_t MasterModbusTasBuffer[ 256 ];
 osStaticThreadDef_t MasterModbusTasControlBlock;
+osThreadId HoldingHandlerHandle;
+uint32_t HoldingHandlerBuffer[ 256 ];
+osStaticThreadDef_t HoldingHandlerControlBlock;
+osThreadId InputHandlerHandle;
+uint32_t InputHandlerBuffer[ 256 ];
+osStaticThreadDef_t InputHandlerControlBlock;
+osMutexId myMutex01Handle;
+osStaticMutexDef_t myMutex01ControlBlock;
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
@@ -62,6 +70,8 @@ osStaticThreadDef_t MasterModbusTasControlBlock;
 
 void SlaveModbusTaskFunction(void const * argument);
 void MasterModbusTaskFunction(void const * argument);
+void HoldingHandlerFunction(void const * argument);
+void InputHandlerFunction(void const * argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
@@ -90,6 +100,10 @@ void MX_FREERTOS_Init(void) {
   /* USER CODE BEGIN Init */
 
   /* USER CODE END Init */
+  /* Create the mutex(es) */
+  /* definition and creation of myMutex01 */
+  osMutexStaticDef(myMutex01, &myMutex01ControlBlock);
+  myMutex01Handle = osMutexCreate(osMutex(myMutex01));
 
   /* USER CODE BEGIN RTOS_MUTEX */
   /* add mutexes, ... */
@@ -116,6 +130,14 @@ void MX_FREERTOS_Init(void) {
   osThreadStaticDef(MasterModbusTas, MasterModbusTaskFunction, osPriorityNormal, 0, 256, MasterModbusTasBuffer, &MasterModbusTasControlBlock);
   MasterModbusTasHandle = osThreadCreate(osThread(MasterModbusTas), NULL);
 
+  /* definition and creation of HoldingHandler */
+  osThreadStaticDef(HoldingHandler, HoldingHandlerFunction, osPriorityNormal, 0, 256, HoldingHandlerBuffer, &HoldingHandlerControlBlock);
+  HoldingHandlerHandle = osThreadCreate(osThread(HoldingHandler), NULL);
+
+  /* definition and creation of InputHandler */
+  //osThreadStaticDef(InputHandler, InputHandlerFunction, osPriorityNormal, 0, 256, InputHandlerBuffer, &InputHandlerControlBlock);
+  //InputHandlerHandle = osThreadCreate(osThread(InputHandler), NULL);
+
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
   /* USER CODE END RTOS_THREADS */
@@ -136,7 +158,8 @@ void SlaveModbusTaskFunction(void const * argument)
   for(;;)
   {
 		eMBPoll();
-    osDelay(2);
+    osDelay(3);
+		taskYIELD();
   }
   /* USER CODE END SlaveModbusTaskFunction */
 }
@@ -154,14 +177,103 @@ void MasterModbusTaskFunction(void const * argument)
   /* Infinite loop */
   for(;;)
   {
-		
-		eMBMasterPoll();
-		eMBMasterPoll();
-    //eMBMasterReqWriteHoldingRegister(ModBusSlaveDefaultDeviceAddr, 10, 0x5678, 100);
-		eMBMasterReqReadHoldingRegister( ModBusSlaveDefaultDeviceAddr, 0, 100, 100 );
-    osDelay(20);
+		// Пытаемся захватить мьютекс (ждём 10 мс)
+     osStatus status = osMutexWait(myMutex01Handle, 100);
+		 if (status == osOK) {		 
+	   
+		     eMBMasterPoll();
+			   eMBMasterPoll();
+	
+		  
+			  // Освобождаем мьюте
+       osMutexRelease(myMutex01Handle);
+		 }
+		 
+		 else
+		 {
+		 
+		 
+		 }
+    osDelay(40);
+		taskYIELD();
   }
   /* USER CODE END MasterModbusTaskFunction */
+}
+
+/* USER CODE BEGIN Header_HoldingHandlerFunction */
+/**
+* @brief Function implementing the HoldingHandler thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_HoldingHandlerFunction */
+void HoldingHandlerFunction(void const * argument)
+{
+  /* USER CODE BEGIN HoldingHandlerFunction */
+	
+	static uint8_t SelectRunFlag = 0;
+  /* Infinite loop */
+  for(;;)
+  {	// Пытаемся захватить мьютекс (ждём 10 мс)
+     osStatus status = osMutexWait(myMutex01Handle, 50);
+		 if (status == osOK) {
+			 if(SelectRunFlag == 0)
+			 {
+			  eMBMasterReqReadHoldingRegister( ModBusSlaveDefaultDeviceAddr, 0, 100, 200 );
+				SelectRunFlag = 1;
+			 }
+			else if(SelectRunFlag == 1)
+			 { 
+			  eMBMasterReqReadInputRegister( ModBusSlaveDefaultDeviceAddr, 0, 50, 200 );
+				SelectRunFlag = 0;
+			 }
+			//Освобождаем мьютекс
+       osMutexRelease(myMutex01Handle);
+		 }
+		 
+		 else
+		 {
+		 
+		 
+		 } 
+			 
+    osDelay(250);
+		taskYIELD();
+  }
+  /* USER CODE END HoldingHandlerFunction */
+}
+
+/* USER CODE BEGIN Header_InputHandlerFunction */
+/**
+* @brief Function implementing the InputHandler thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_InputHandlerFunction */
+void InputHandlerFunction(void const * argument)
+{
+  /* USER CODE BEGIN InputHandlerFunction */
+  /* Infinite loop */
+  for(;;)
+  {// Пытаемся захватить мьютекс (ждём 10 мс)
+//     osStatus status = osMutexWait(myMutex01Handle, 50);
+//		 if (status == osOK) {
+//		  eMBMasterPoll();	
+//			 eMBMasterReqReadInputRegister( ModBusSlaveDefaultDeviceAddr, 0, 20, 100 );
+//			 eMBMasterPoll();
+//				  // Освобождаем мьютекс
+//       osMutexRelease(myMutex01Handle);
+//		 }
+//		 
+//		 else
+//		 {
+//		 
+//		 
+//		 } 
+			 
+    osDelay(500);
+  }
+  /* USER CODE END InputHandlerFunction */
 }
 
 /* Private application code --------------------------------------------------*/
