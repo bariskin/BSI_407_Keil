ca204b930e7b2836be41b6ea3a2952afdfa164cf
bariskin <chiurcciu@mail.ru>
2025-07-30 17:22:42 +0300

    adde Display driver directory and TemplateFilesDirectory

---
+++ DisplayDriver/Inc/DisplayDriver.h
@@ -0,0 +1,42 @@
+/**
+******************************************************************************
+* @file      DisplayDriver.h
+* @author    OnWert
+* @version   
+* @brief     This file contains defines and all the functions prototypes for the DisplayDriver.c
+******************************************************************************
+*/
+#ifndef _DISPLAY_DRIVER_H
+#define _DISPLAY_DRIVER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+  
+ /* ------------------------Includes ----------------------------------*/
+ #include "stm32f4xx_hal.h"
+  /** @defgroup display_driver
+  * @{
+  */
+  
+  /**
+  * @}
+  */
+ /* ------------------------Defines ----------------------------------*/
+    
+ /* ------------------------External variables -------------------------*/
+  
+
+  /** @addtogroup display_driver
+  * @{
+  */
+
+  /**
+  * @}
+  */
+  
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* _DISPLAY_DRIVER_H */
---
+++ DisplayDriver/Src/DisplayDriver.c
@@ -0,0 +1,23 @@
+/**
+******************************************************************************
+* @file      DisplayDriver.c
+* @author    OnWert
+* @version   
+* @brief     This file provides common functions for the  display Nexton 
+*           
+*/       
+/* ------------------------Includes ----------------------------------*/
+#include "DisplayDriver.h"
+
+/* ------------------------External variables -------------------------*/
+
+/* ------------------------Global variables----------------------------*/
+
+/* ------------------------Locale variables----------------------------*/
+
+/* ------------------------Functions-----------------------------------*/
+
+/************************ (C) COPYRIGHT  OnWert *****END OF FILE****/
+
+
+
